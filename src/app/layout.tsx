29d3b2f2f6795658b402b153c8e1578125a6b328
Md. Fayzur Rahman <mdfayzurrahman030@gmail.com>
2026-09-24 01:21:52 +0600

    style: add Google font configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Geist, Geist_Mono, Oswald } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/shared/Navbar'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+// const geistSans = Geist({
+//   variable: '--font-geist-sans',
+//   subsets: ['latin'],
+// })
+const oswald = Oswald({
   subsets: ['latin'],
+  variable: '--font-oswald',
 })
 
 const geistMono = Geist_Mono({
@@ -22,7 +26,7 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
     <html
       lang='en'
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${oswald.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className='min-h-full flex flex-col'>
         <Navbar />
--- src/components/workouts/HeroSection.tsx
+++ src/components/workouts/HeroSection.tsx
@@ -6,19 +6,23 @@ const HeroSection = () => {
     <div className='max-w-7xl mx-auto px-4 md:px-4 lg:px-0 mt-6 md:mt-10 lg:mt-12'>
       <div className='bg-[#222630]/40 p-6 md:p-8 lg:p-14 rounded-lg border border-gray-50/15 shadow flex flex-col md:flex-row gap-8 md:gap-6 justify-between items-center'>
         <div className='space-y-4 md:space-y-6 lg:space-y-6 w-full md:w-1/2 lg:w-auto text-center md:text-start'>
-          <h3 className='text-[#C2F800] font-bold text-sm'>WORKOUT LIBRARY</h3>
+          <p className='text-[#C2F800] font-bold text-sm font-inter'>
+            WORKOUT LIBRARY
+          </p>
 
-          <h1 className='text-3xl sm:text-4xl md:text-4xl lg:text-5xl max-w-full md:max-w-5xl font-oswald font-extrabold leading-tight tracking-tight'>
+          <h1 className='text-3xl sm:text-4xl md:text-4xl lg:text-5xl max-w-full md:max-w-xl font-oswald font-extrabold leading-tight '>
             TRAIN WITH INTENT. LOG EVERY SET.
           </h1>
 
-          <p className='text-xs md:text-sm text-[#9CA3AF] max-w-96 font-normal leading-6 '>
+          <p
+            className={`text-xs md:text-sm text-[#9CA3AF] max-w-96 font-inter font-norm leading-6 `}
+          >
             FitLog is a dark, no-nonsense gym companion: pick a lift, lock it
             into today&apos;s plan, and watch the week&apos;s work add up.
           </p>
 
           <button className='bg-[#C2F800] btn px-6 text-[#000000] text-sm  w-full sm:w-auto'>
-            BROWSE WORKOUTS
+            <a href='#library'>BROWSE WORKOUTS</a>
           </button>
         </div>
 
