c7b1fe8deb5ca19880bb62c12906503bdb46d16e
Md. Fayzur Rahman <mdfayzurrahman030@gmail.com>
2026-09-25 18:31:31 +0600

    Mark as Done button implement

--- src/components/button/MarkAsDoneBtn.tsx
+++ src/components/button/MarkAsDoneBtn.tsx
@@ -11,12 +11,8 @@ interface PlanProps {
 }
 const MarkAsDoneBtn = ({ plan }: PlanProps) => {
   const { setWorkoutPlans } = useContext(WorkoutContext)
-  const handelMarkAsDone = () => {
-    setWorkoutPlans((prev) =>
-      prev.map((workout) =>
-        workout.id === plan.id ? { ...workout, isDone: true } : workout
-      )
-    )
+  const handelMarkAsDone = (id: number) => {
+    setWorkoutPlans((prev) => prev.filter((item) => item.id !== id))
 
     showSuccessToast(`${plan.name} marked as done!`)
   }
@@ -24,7 +20,7 @@ const MarkAsDoneBtn = ({ plan }: PlanProps) => {
   return (
     <button
       className={`flex items-center gap-2 rounded-full py-3 bg-[#C2F800] px-3 cursor-pointer  text-xs font-medium text-black transition hover:bg-[#b0e200] sm:flex-none sm:px-4 sm:text-sm`}
-      onClick={handelMarkAsDone}
+      onClick={() => handelMarkAsDone(plan.id)}
     >
       <Check size={16} /> Mark as Done
     </button>
